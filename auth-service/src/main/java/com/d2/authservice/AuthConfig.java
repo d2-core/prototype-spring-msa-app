0f71fd021da360d302e1ab27a313abf8568595ff
d2-core <dohaktae88@gmail.com>
2024-11-24 23:01:07 +0900

    [AUTH] jpa, core config 추가

--- auth-service/src/main/java/com/d2/authservice/AuthConfig.java
+++ auth-service/src/main/java/com/d2/authservice/AuthConfig.java
@@ -2,7 +2,8 @@ package com.d2.authservice;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-@ComponentScan("com.d2.cor")
+
+@ComponentScan({"com.d2.core", "come.d2.authservice"})
 @Configuration
 public class AuthConfig {
 }
