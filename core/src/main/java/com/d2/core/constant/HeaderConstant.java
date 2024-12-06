caf0d8c49b7af0b4f41e13c1bde0f45e86dd2495
d2-core <dohaktae88@gmail.com>
2024-12-06 16:40:34 +0900

    [CORE] HeaderConstant Refresh 추가

--- core/src/main/java/com/d2/core/constant/HeaderConstant.java
+++ core/src/main/java/com/d2/core/constant/HeaderConstant.java
@@ -2,6 +2,7 @@ package com.d2.core.constant;
 
 public class HeaderConstant {
 	public static final String X_D2_PREFIX = "X-D2";
+	public static final String X_D2_REFRESH = "X-D2-AUTH-REFRESH";
 	public static final String X_D2_REQUEST_UUID = "X-D2-REQUEST-UUID";
 	public static final String X_D2_AUTH_ID = "X-D2-AUTH-ID";
 	public static final String X_D2_AUTH_ROLE = "X-D2-AUTH-ROLE";
