f639ee008646313fd9013e3151037488870745cc
d2-core <dohaktae88@gmail.com>
2024-12-06 16:43:11 +0900

    File Clean

--- auth-service/src/main/java/com/d2/authservice/AuthConfig.java
+++ auth-service/src/main/java/com/d2/authservice/AuthConfig.java
@@ -3,7 +3,7 @@ package com.d2.authservice;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan({"com.d2.core", "come.d2.authservice"})
+@ComponentScan({"com.d2.core", "com.d2.authservice"})
 @Configuration
 public class AuthConfig {
 }
