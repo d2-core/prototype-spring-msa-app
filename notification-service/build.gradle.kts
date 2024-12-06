a233244d01ff134a00b2a38c6bf068bfd3b476ec
d2-core <dohaktae88@gmail.com>
2024-12-06 16:39:48 +0900

    ComponentScan 수정

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -1,8 +1,6 @@
 val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
 val jar: Jar by tasks
 
-project.version = "1.0.0"
-
 bootJar.enabled = true
 jar.enabled = false
 
--- notification-service/src/main/java/com/d2/notificationservice/NotificationConfig.java
+++ notification-service/src/main/java/com/d2/notificationservice/NotificationConfig.java
@@ -3,7 +3,7 @@ package com.d2.notificationservice;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan("com.d2.core")
+@ComponentScan({"com.d2.core", "com.d2.notificationservice"})
 @Configuration
 public class NotificationConfig {
 }
