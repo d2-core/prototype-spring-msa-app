d0e2a1f75e5c9530851ea988e27dedafda05d180
d2-core <dohaktae88@gmail.com>
2025-01-01 23:55:11 +0900

    [CORE] kafka, jpa h2 의존성 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,8 @@ bootJar.enabled = false
 jar.enabled = true
 
 dependencies {
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.691")
 }
