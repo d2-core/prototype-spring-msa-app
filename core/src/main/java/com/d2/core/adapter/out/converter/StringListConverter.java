69bdbd1992f427aed9d1c082a4cbfffd3170107c
d2-core <dohaktae88@gmail.com>
2025-01-01 23:55:36 +0900

    [CORE] StringListConverter file 정리

--- core/src/main/java/com/d2/core/adapter/out/converter/StringListConverter.java
+++ core/src/main/java/com/d2/core/adapter/out/converter/StringListConverter.java
@@ -1,13 +1,11 @@
 package com.d2.core.adapter.out.converter;
 
-import java.util.List;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 
 public class StringListConverter extends JsonArrayConverter<String> {
 
 	public StringListConverter() {
-		super(new TypeReference<List<String>>() {
+		super(new TypeReference<>() {
 		});
 	}
 }
