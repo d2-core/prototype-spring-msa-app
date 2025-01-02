b8c16d7cf43fa47a55efb97f862a054ff97abbc7
d2-core <dohaktae88@gmail.com>
2025-01-03 07:17:58 +0900

    [CORE] R2ExternalSystem image upload upload unique name logic 변경 및 단일 upload 추가

--- core/src/main/java/com/d2/core/application/port/out/ObjectStoragePort.java
+++ core/src/main/java/com/d2/core/application/port/out/ObjectStoragePort.java
@@ -6,7 +6,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ObjectStoragePort {
 
+	String uploadImage(MultipartFile imageFile);
+
 	String uploadImage(String preUrl, MultipartFile imageFile);
+
+	List<String> uploadImages(List<MultipartFile> imageFiles);
 
 	List<String> uploadImages(List<String> preImageUrls, List<MultipartFile> imageFiles);
 }
