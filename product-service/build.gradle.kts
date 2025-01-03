b5e66268a164aae6292f27881801bf882de96296
d2-core <dohaktae88@gmail.com>
2025-01-03 22:25:46 +0900

    [PRODUCT] kafka 의존성 추가

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -7,6 +7,7 @@ bootJar.enabled = true
 jar.enabled = false
 
 dependencies {
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
