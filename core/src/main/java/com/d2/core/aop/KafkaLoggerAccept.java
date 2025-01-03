d66ae73177c604c2aaaa7bbbaab74087350a91c8
d2-core <dohaktae88@gmail.com>
2025-01-03 22:23:10 +0900

    [CORE] KafkaLogger name change -> KafkaLoggerAccept

--- core/src/main/java/com/d2/core/aop/KafkaLoggerAccept.java
+++ core/src/main/java/com/d2/core/aop/KafkaLoggerAccept.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 @RequiredArgsConstructor
 @Component
-public class KafkaLogger {
+public class KafkaLoggerAccept {
 
 	private final InternalKafkaProducer internalKafkaProducer;
 
